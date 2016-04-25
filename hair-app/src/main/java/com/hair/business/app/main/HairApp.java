e603b2bbeeff7e49c307cb7ba3f766495a7e1efd
koredyte <koredyte@8b9e3e8f-249f-442b-a663-5b5fd854ee73>
2016-04-25 02:16:11 +0000

    initial logging integration.

---
+++ hair-app/src/main/java/com/hair/business/app/main/HairApp.java
@@ -0,0 +1,18 @@
+package com.hair.business.app.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by olukoredeaguda on 25/04/2016.
+ */
+public class HairApp {
+    static final Logger logger = LoggerFactory.getLogger(HairApp.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting hair application");
+
+        //AnnotationConficApplicationContext context = new AnnotationConfigApplicationContext();
+
+    }
+}
