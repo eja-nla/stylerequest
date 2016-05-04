79f57e70af8d97535c9c1ca0d14c0bf827d9c52a
koredyte <koredyte@8b9e3e8f-249f-442b-a663-5b5fd854ee73>
2016-05-04 20:48:17 +0000

    : integrated with jetty : added image service

--- trunk/core/beans/src/main/java/com/hair/business/dao/constants/EntityConstants.java
+++ trunk/core/beans/src/main/java/com/hair/business/dao/constants/EntityConstants.java
@@ -8,7 +8,7 @@ public class EntityConstants {
 
     public static final long VERSION_DEFAULT =1L;
 
-    public static final String STYLE_REQUEST_INDEX = "styleRequests";
+    public static final String STYLE_REQUEST_INDEX = "styles";
     public static final String CUSTOMER_INDEX = "customers";
     public static final String MERCHANT_INDEX = "merchants";
     public static final String PAYMENT_INDEX = "payments";
@@ -16,7 +16,7 @@ public class EntityConstants {
     public static final String REVIEWS_INDEX = "reviews";
     public static final String IMAGES_INDEX = "images";
 
-    public static final String STYLE_REQUEST_TYPE = "styleRequest_type_1";
+    public static final String STYLE_REQUEST_TYPE = "styles_type_1";
     public static final String CUSTOMER_TYPE = "customers_type_1";
     public static final String MERCHANT_TYPE = "merchants_type_1";
     public static final String PAYMENT_TYPE = "payments_type_1";
