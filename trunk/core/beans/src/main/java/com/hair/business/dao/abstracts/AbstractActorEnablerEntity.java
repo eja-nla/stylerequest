e97b94ef0c51a1e21b3086b236dec4373453bafc
koredyte <koredyte@8b9e3e8f-249f-442b-a663-5b5fd854ee73>
2016-05-25 00:13:33 +0000

    moving spring impl off trunk

--- trunk/core/beans/src/main/java/com/hair/business/dao/abstracts/AbstractActorEnablerEntity.java
+++
@@ -1,9 +0,0 @@
-package com.hair.business.dao.abstracts;
-
-/**
- * Abstract actor enabler e.g. payments, location, reviews etc.
- *
- * Created by Olukorede Aguda on 25/04/2016.
- */
-public abstract class AbstractActorEnablerEntity extends AbstractPersistenceEntity {
-}
--- trunk/core/services/src/main/java/com/hair/business/services/client/HairWebclient.java
+++
@@ -1,7 +0,0 @@
-package com.hair.business.services.client;
-
-/**
- * Created by Olukorede Aguda on 26/04/2016.
- */
-public class HairWebclient {
-}
