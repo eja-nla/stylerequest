b9bf080ac6bd3018764893bcc88df4a0acde167c
koredyte <koredyte@8b9e3e8f-249f-442b-a663-5b5fd854ee73>
2016-06-20 17:03:31 +0000

    Added ofy integration

---
+++ core/dao-datastore/src/main/java/com/hair/business/dao/datastore/abstractRepository/ObjectifyRepository.java
@@ -0,0 +1,11 @@
+package com.hair.business.dao.datastore.abstractRepository;
+
+/**
+ * Objectify Repository interface
+ *
+ *
+ * Created by Olukorede Aguda on 25/04/2016.
+ */
+public interface ObjectifyRepository extends Repository {
+
+}
