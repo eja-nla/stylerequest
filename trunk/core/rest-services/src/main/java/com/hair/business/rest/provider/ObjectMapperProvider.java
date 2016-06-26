dde3f6745d184c42aee12a3cbd6de5c5490c927c
koredyte <koredyte@8b9e3e8f-249f-442b-a663-5b5fd854ee73>
2016-06-26 23:20:20 +0000

    Added functional merchant servlet and bound jackson messagebodyreader/writer.

--- trunk/core/rest-services/src/main/java/com/hair/business/rest/provider/ObjectMapperProvider.java
+++ trunk/core/rest-services/src/main/java/com/hair/business/rest/provider/ObjectMapperProvider.java
@@ -1,5 +1,6 @@
 package com.hair.business.rest.provider;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -19,6 +20,7 @@ public class ObjectMapperProvider implements Provider<ObjectMapper> {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         return mapper;
     }
 }
