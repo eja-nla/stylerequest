80ee5de464a3e68ef80f0cc0abad7d12065f412d
koredyte <koredyte@8b9e3e8f-249f-442b-a663-5b5fd854ee73>
2016-11-13 01:29:53 +0000

    Fix issue where Jersey 1 and 2 are added to classpath (I erroneously added silly test jersey 2 deps)

--- trunk/core/util/src/main/java/com/x/business/utilities/Assert.java
+++ trunk/core/util/src/main/java/com/x/business/utilities/Assert.java
@@ -1,5 +1,7 @@
 package com.x.business.utilities;
 
+import com.x.business.exception.EntityNotFoundException;
+
 /**
  * Created by Olukorede Aguda on 22/06/2016.
  */
@@ -8,6 +10,12 @@ public class Assert {
     public static void notNull(Object o, String m){
         if(null == o){
             throw new IllegalArgumentException(m);
+        }
+    }
+
+    public static void isFound(Object entity, String message){
+        if(null == entity){
+            throw new EntityNotFoundException(message);
         }
     }
 }
