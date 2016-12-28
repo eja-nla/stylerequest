a61288f116f4a065e21aae70e4add106819d4150
koredyte <koredyte@8b9e3e8f-249f-442b-a663-5b5fd854ee73>
2016-12-28 16:02:33 +0000

    deactivate APNS test until feature is activated

--- trunk/core/services/src/test/java/com/hair/business/services/customer/CustomerServiceTest.java
+++ trunk/core/services/src/test/java/com/hair/business/services/customer/CustomerServiceTest.java
@@ -22,8 +22,12 @@ import com.x.business.scheduler.TaskQueue;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Olukorede Aguda on 23/06/2016.
@@ -40,6 +44,12 @@ public class CustomerServiceTest extends AbstractServicesTestBase {
     public void setUp(){
         repository = injector.getInstance(Repository.class);
         cs = new CustomerServiceImpl(repository, queue1, queue2);
+
+        try {
+            System.setProperty("SENDGRID_NEW_STYLE_EMAIL_TEMPLATE_FILE", new File("src/test/resources/newStyleTemplate.json").getCanonicalPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -52,10 +62,23 @@ public class CustomerServiceTest extends AbstractServicesTestBase {
         StyleRequest styleRequest = cs.placeStyleRequest(style.getId(), customer.getId(), m.getId(), DateTime.now());
 
         verify(queue1, times(1)).add(any(Notification.class));
-        verify(queue2, times(1)).add(any(SendPushNotificationToApnsTask.class));
 
         assertThat(styleRequest, is(notNullValue()));
-        assertThat(styleRequest.getStyle().getRequestCount(), is(new Long(1)));
+        assertThat(styleRequest.getStyle().getRequestCount(), is(1L));
+
+    }
+
+    @Ignore("until apns feature activation")
+    @Test
+    public void testPlaceStyleRequestWithAPNS() throws Exception {
+        Style style = createStyle();
+        Customer customer = createCustomer();
+        Merchant m = createMerchant();
+        repository.saveFew(style, customer, m);
+
+        cs.placeStyleRequest(style.getId(), customer.getId(), m.getId(), DateTime.now());
+
+        verify(queue2, times(1)).add(any(SendPushNotificationToApnsTask.class));
 
     }
 
