d359171cdcc68f1e930bf2954a7802a712f06ed4
koredyte <koredyte@8b9e3e8f-249f-442b-a663-5b5fd854ee73>
2017-02-08 21:26:12 +0000

    Add update style API

--- trunk/core/rest-services/src/main/java/com/hair/business/rest/MvcConstants.java
+++ trunk/core/rest-services/src/main/java/com/hair/business/rest/MvcConstants.java
@@ -18,13 +18,13 @@ public class MvcConstants {
     public static final String INFO="/info";
 
     public static final String CREATE="/create";
-
-    public static final String CREATE_MERCHANT_ENDPOINT = CREATE + "/merchant";
-    public static final String CREATE_CUSTOMER_ENDPOINT = CREATE + "/customer";
-    public static final String PUBLISH_STYLE_ENDPOINT = CREATE + "/style";
-
     public static final String STYLE_URI="/style";
-    public static final String UPDATE_STYLE_ENDPOINT = STYLE_URI + "/update";
+
+    public static final String CREATE_MERCHANT_ENDPOINT = CREATE + CUSTOMER_URI;
+    public static final String CREATE_CUSTOMER_ENDPOINT = CREATE + CUSTOMER_URI;
+    public static final String PUBLISH_STYLE_ENDPOINT = CREATE + STYLE_URI;
+
+    public static final String UPDATE_STYLE_ENDPOINT = "/update";
 
     public static final String STYLE_REQUEST_PATH = "/request/style";
 
@@ -33,6 +33,5 @@ public class MvcConstants {
     public static final String EMAIL="email";
 
     public static final String ADMIN_URI="admin";
-
 
 }
